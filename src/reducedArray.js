0b5433c7903a6a1315d6eac2811d1590fd52d420
mtslck <mats.loock@lnu.se>
2016-09-16 13:02:09 +0200

    Initial commit

---
+++ src/reducedArray.js
@@ -0,0 +1,24 @@
+/**
+ * Module for reducedArray.
+ *
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+/**
+ * Returns the sum of all object's numbers.
+ *
+ * @param {Object[]} data - An array of objects to analyze.
+ * @param {Number} data[].nr - The number.
+ * @returns {Number} - The total sum of the nr property of the objects in the provided array.
+ */
+let getTotal = function(data) {
+
+  // TODO: Write your code here!
+
+};
+
+module.exports.getTotal = getTotal;
